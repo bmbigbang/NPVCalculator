949efcc484f39aaab0aa36da7e00a5f73b4342fa
Ardavan Shafiee <bm.bigbang@gmail.com>
2025-07-13 13:43:08 +0100

    run function calculations based on the lower/upper bounds and discount increments add initialInvestment param

--- NPVCalculatorAPI/Controllers/NPVCalculationController.cs
+++ NPVCalculatorAPI/Controllers/NPVCalculationController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using NPVCalculatorAPI.Models;
+using System.Collections.Concurrent;
+using System.Text.Json;
+using System.Text;
 using NPVCalculatorFunction.Models;
 
 namespace NPVCalculatorAPI.Controllers;
@@ -9,15 +12,68 @@ namespace NPVCalculatorAPI.Controllers;
 public class NPVCalculationController : ControllerBase
 {
     private readonly ILogger<NPVCalculationController> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly string _functionAppUrl = "http://localhost:7073/api/CalculateNpv";
 
-    public NPVCalculationController(ILogger<NPVCalculationController> logger)
+    public NPVCalculationController(ILogger<NPVCalculationController> logger, HttpClient httpClient)
     {
         _logger = logger;
+        _httpClient = httpClient;
     }
 
     [HttpPost(Name = "CalculateNPV")]
-    public IEnumerable<double> GetCalculateNPV([FromBody] NPVBoundsRequest boundsRequest)
+    public async Task<IEnumerable<double>> GetCalculateNPV([FromBody] NPVBoundsRequest boundsRequest)
     {
-        return [];
+        var discountRates = new List<double>();
+        var currentRate = boundsRequest.LowerBoundDiscountRate;
+        
+        // Generate all discount rates to test
+        while (currentRate <= boundsRequest.UpperBoundDiscountRate)
+        {
+            discountRates.Add(currentRate);
+            currentRate += boundsRequest.DiscountRateIncrement;
+        }
+
+        // Thread-safe collection to store results
+        var npvResults = new ConcurrentBag<(double DiscountRate, double NPV)>();
+
+        // Create tasks for parallel execution
+        var tasks = discountRates.Select(async discountRate =>
+        {
+            try
+            {
+                var requestData = new
+                {
+                    initialInvestment = boundsRequest.InitialInvestment,
+                    discountRate = discountRate,
+                    cashFlows = boundsRequest.CashFlows
+                };
+
+                var jsonContent = JsonSerializer.Serialize(requestData);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync(_functionAppUrl, content);
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var npvResponse = JsonSerializer.Deserialize<NpvResponse>(responseContent);
+
+                npvResults.Add((discountRate, npvResponse!.NpvNetPresentValue));
+                
+                _logger.LogInformation($"NPV calculated for discount rate {discountRate}: {npvResponse.NpvNetPresentValue}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error calculating NPV for discount rate {discountRate}");
+            }
+        });
+
+        // Execute all tasks concurrently
+        await Task.WhenAll(tasks);
+
+        // Return results ordered by discount rate
+        return npvResults
+            .OrderByDescending(x => x.DiscountRate)
+            .Select(x => x.NPV);
     }
 }
--- NPVCalculatorAPI/Models/NPVBoundsRequestRecord.cs
+++ NPVCalculatorAPI/Models/NPVBoundsRequestRecord.cs
@@ -6,4 +6,5 @@ public record NPVBoundsRequest(
     [property: JsonPropertyName("lowerBoundDiscountRate")] double LowerBoundDiscountRate, 
     [property: JsonPropertyName("upperBoundDiscountRate")] double UpperBoundDiscountRate, 
     [property: JsonPropertyName("discountRateIncrement")] double DiscountRateIncrement,
-    [property: JsonPropertyName("cashFlows")] double[] CashFlows);
+    [property: JsonPropertyName("cashFlows")] double[] CashFlows,
+    [property: JsonPropertyName("initialInvestment")] double InitialInvestment);
